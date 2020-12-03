2341073f00c38221a911bbc0bccdcde3a7d529cc
newcoder18-create <64659521+newcoder18-create@users.noreply.github.com>
2020-12-03 17:12:40 +0530

    Changes

---
+++ src/qapi.js
@@ -0,0 +1,25 @@
+export default {
+	"pages": {
+		"root": {
+			"id": "root",
+			"pageUrl": "root",
+			"name": "root",
+			"children": [
+				"5fc8cc48e563f3001ecb5b5e",
+				"5fc8cc48e563f3001ecb5b60"
+			]
+		},
+		"5fc8cc48e563f3001ecb5b5e": {
+			"id": "5fc8cc48e563f3001ecb5b5e",
+			"name": "404",
+			"pageUrl": "404"
+		},
+		"5fc8cc48e563f3001ecb5b60": {
+			"id": "5fc8cc48e563f3001ecb5b60",
+			"name": "index",
+			"pageUrl": "index"
+		}
+	},
+	"mode": "production",
+	"projectType": "create-react-app"
+}
